e966b28ae3d307db011db2079dc551048602c24c
vibha <vibhauc@gmail.com>
2022-02-24 16:17:47 +0530

    [Vibha]Added:Created a simple linked list

---
+++ DataStructure/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DataStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Data Structure Program");
+                        
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();            
+        }
+    }
+}
